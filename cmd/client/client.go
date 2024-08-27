1dbc1da0f8ddff1f9c4c669ee7d875972c2e6af3
liyouxin <liyouxina@gmail.com>
2024-08-27 18:40:47 +0800

    more

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"flag"
-	"github.com/liyouxina/tunnel/pkg/agent"
+	"net"
+
 	"github.com/liyouxina/tunnel/pkg/logger"
 )
 
-var tunnelServer = flag.String("tunnelServer", "localhost:8091", "tunnelServer")
-var targetServer = flag.String("targetServer", "localhost:8080", "targetServer")
-var maxAgentCnt = flag.Int("maxAgentCnt", 100, "maxAgentCnt")
-
+var waiCon net.Conn
+var neiCon net.Conn
 var log = logger.Logger
 
 func init() {
@@ -17,7 +16,35 @@ func init() {
 }
 
 func main() {
-	agent.StartAgents(*tunnelServer, *targetServer, *maxAgentCnt)
-	log.Infof("启动成功")
-	select {}
+	var err error
+	waiCon, err = net.Dial("tcp", "127.0.0.1:8080")
+	neiCon, err = net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Errorf("dial failed %s", err)
+	}
+
+	go func() {
+		for {
+			buffer := make([]byte, 1024)
+			n, err := waiCon.Read(buffer)
+			if err != nil {
+				log.Warnf("Failed to read nei tunnel connection %v", err)
+			}
+
+			_, err = neiCon.Write(buffer[:n])
+			if err != nil {
+				log.Warnf("Failed to write wai tunnel connection %v", err)
+			}
+		}
+	}()
+
 }
+
+GET / HTTP/1.1
+Host: 127.0.0.1:8080
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Connection: close
+Upgrade-Insecure-Requests: 1
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,74 +2,81 @@ package main
 
 import (
 	"flag"
-	"github.com/gin-gonic/gin"
-	"github.com/liyouxina/tunnel/pkg/protocal"
-	"github.com/liyouxina/tunnel/pkg/tunnel"
 	"net"
-	"sync"
 
 	"github.com/liyouxina/tunnel/pkg/logger"
 )
 
-var serverPort = flag.String("serverPort", "8090", "serverPort")
 var tunnelPort = flag.String("tunnelPort", "8091", "tunnelPort")
-
+var waiCon net.Conn
+var neiCon net.Conn
 var log = logger.Logger
-var proto = protocal.HTTPProtocol{}
-var taskPool = make(chan *protocal.Task, 100000)
 
 func init() {
 	flag.Parse()
 }
 
 func main() {
-	startTunnelServer()
-	startHTTPServer()
-}
-
-func startTunnelServer() {
-	go func() {
-		listener, err := net.Listen("tcp", ":"+*tunnelPort)
+	var err error
+	listener, err := net.Listen("tcp", ":"+*tunnelPort)
+	if err != nil {
+		log.Fatalf("start tunnel server failed %v", err)
+		return
+	}
+	log.Infof("start tunnel server success %v", *tunnelPort)
+	for {
+		neiCon, err = listener.Accept()
 		if err != nil {
-			log.Fatalf("start tunnel server failed %v", err)
-			return
+			log.Warnf("Failed to accept tunnel connection %v", err)
+			continue
 		}
-		log.Infof("start tunnel server success %v", *tunnelPort)
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				log.Warnf("Failed to accept tunnel connection %v", err)
-				continue
+
+		go func() {
+			for {
+				buffer := make([]byte, 1024)
+				n, err := neiCon.Read(buffer)
+				if err != nil {
+					log.Warnf("Failed to read nei tunnel connection %v", err)
+				}
+				_, err = waiCon.Write(buffer[:n])
+				if err != nil {
+					log.Warnf("Failed to write wai tunnel connection %v", err)
+				}
 			}
-			tunnel.NewTunnel(conn, proto).Run(taskPool)
-			log.Infof("get tunnel %s", conn.RemoteAddr())
-		}
-	}()
+
+		}()
+	}
+
 }
 
-func startHTTPServer() {
-	server := gin.Default()
-	server.Any("/*path", func(c *gin.Context) {
-		body := make([]byte, 8096)
-		n, _ := c.Request.Body.Read(body)
-		bodyString := string(body[:n])
-		headersString := ""
-		for k, v := range c.Request.Header {
-			headersString = headersString + k + protocal.HEADER_SPLIT + v[0] + protocal.HEADER_TAIL
+func server() {
+	var err error
+	listener, err := net.Listen("tcp", ":10001")
+	if err != nil {
+		log.Fatalf("start tunnel server failed %v", err)
+		return
+	}
+	log.Infof("start tunnel server success %v", *tunnelPort)
+	for {
+		waiCon, err = listener.Accept()
+		if err != nil {
+			log.Warnf("Failed to accept tunnel connection %v", err)
+			continue
 		}
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		task := protocal.Task{
-			Headers: headersString,
-			Url:     c.Request.RequestURI,
-			Body:    bodyString,
-			Method:  c.Request.Method,
-			Wg:      &wg,
-		}
-		taskPool <- &task
-		wg.Wait()
-		c.Status(task.ResStatus)
-		_, _ = c.Writer.Write([]byte(task.ResBody))
-	})
-	_ = server.Run("0.0.0.0:" + *serverPort)
+
+		go func() {
+			for {
+				buffer := make([]byte, 1024)
+				n, err := waiCon.Read(buffer)
+				if err != nil {
+					log.Warnf("Failed to read wai tunnel connection %v", err)
+				}
+				_, err = neiCon.Write(buffer[:n])
+				if err != nil {
+					log.Warnf("Failed to write nei tunnel connection %v", err)
+				}
+			}
+
+		}()
+	}
 }
